1f7bc64e42a9e70801cd0b1d1dd9f2c184dd7060
Markus Afonso <mafonso4@my.bcit.ca>
2022-11-08 16:15:59 -0800

    arrays

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,3 +3,14 @@ do {
   prompt("what is you name?");
 } while (!isNaN(Number(input)));
 
+let fruits = ["Apple", "Banana"];
+let pushResult = fruits.push("Orange");
+console.log(popResult);
+
+const person = {
+  name: ["Bob", "Smith"],
+  age: 32,
+  gender: "male",
+  hobbies: ["music", "skiing"],
+};
+console.log(person.name[1]);
